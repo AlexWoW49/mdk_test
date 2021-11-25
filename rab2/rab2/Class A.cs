716c219261cb3f7f8d1a21eff90843f92ec32a51
Alex <qazxw.89@mail.ru>
2021-11-25 17:21:00 +0300

    rab2

---
+++ rab2/rab2/Class A.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rab2
+{
+    class A
+    {
+        protected List<double> x;
+
+        public List<double> Solution(double a, double b) {
+            if (a == 0)
+            {
+                throw new Exception("Неправильно, попробуй еще раз!!!");
+            }
+            return x = new List<double>
+            {
+                -b / a
+            };
+            //Console.WriteLine("x= {0}\n", x);
+            
+        }
+    }
+}
---
+++ rab2/rab2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> listB = new B().Solution(1, 8, 0);
+            Console.ReadKey();
+            
+        }
+    }
+}
